005cdbf9fb8470b75dc01b9a1e0caf1b709b8134
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-20 13:01:22 +0530

    Find_Frequancy_Of_Words

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1__Find_Frequancy_Of_Words
+{
+    class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public void Add(K key, V value)
+        {
+            //for Adding Elements In HashTable
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);//will add elements in linkedlist of key value pair
+        }
+
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+
+        public V Get(K key)
+        {
+            //Get Value Of The Key Given
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+    }
+    public struct KeyValue<k, v>
+    {
+        //To Store The Given Key And Value
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+    internal class HT1
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "Be");
+            hash.Add("2", "Or");
+            hash.Add("3", "Not");
+            hash.Add("4", "To");
+            hash.Add("5", "Be");
+            string hash0 = hash.Get("0");
+            Console.WriteLine("0th index value: " + hash0);
+            string hash1 = hash.Get("1");
+            Console.WriteLine("1st index value: " + hash1);
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2nd index value: " + hash2);
+        }
+    }
+}
